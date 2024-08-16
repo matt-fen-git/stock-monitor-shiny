55146ecc7ccced239fe07bbc0553094d1634e423
Matthew Fenech <matthewfenech879@msn.com>
2024-08-16 15:21:42 +0200

    'working'

--- scripts/global.R
+++ scripts/global.R
@@ -10,13 +10,13 @@ library(tidyr)
 library(dplyr)
 
 # Read the content of the text file and convert it to Markdown
-rsi_content <- suppressWarnings(readLines("~/Kaggle Projects/Stock Monitor/Descriptions/rsi_info.txt"))
+rsi_content <- suppressWarnings(readLines("~/Kaggle Projects/shiny-stock-monitor/Descriptions/rsi_info.txt"))
 rsi_content <- paste(rsi_content, collapse = "\n")
 
 # Read Data
-stock_data_list <- readRDS("~/Kaggle Projects/Stock Monitor/data/stock_data_list.rds")
+stock_data_list <- readRDS("~/Kaggle Projects/shiny-stock-monitor/data/stock_data_list.rds")
 
-all_dips_data <- data.table::fread("~/Kaggle Projects/Stock Monitor/data/all_dips_data.csv") %>% as.data.frame()
+all_dips_data <- data.table::fread("~/Kaggle Projects/shiny-stock-monitor/data/all_dips_data.csv") %>% as.data.frame()
 all_dips_data$date <- as.Date(all_dips_data$date)
 
 print("I ran this")
